2e6773dfd38644ef4f32f7e85ade5602da2ae2d1
LexieKwon <kwonlexie@gmail.com>
2021-09-12 09:58:13 -0500

    Fixed fatal error

--- trantest.py
+++ trantest.py
@@ -5,12 +5,9 @@ import win32gui
 import movement
 import random
 from pygame import mixer  # Load the popular external library
-
 # mixer.init()
 # mixer.music.load('C:/Users/Clown/Downloads/Rick_Roll.mp3')
 # mixer.music.play()
-
-
 pygame.init()
 flags = pygame.NOFRAME
 screen = pygame.display.set_mode((0,0), flags)
@@ -20,15 +17,12 @@ goal = list(random.choice(pointarr))
 done = False
 fuchsia = (255, 0, 128)  # Transparency color
 dark_red = (139, 0, 0)
-
 # Create layered window
 hwnd = pygame.display.get_wm_info()["window"]
 win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE,
                        win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)
 # Set window transparency color
 win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*fuchsia), 0, win32con.LWA_COLORKEY)
-
-
 rev = pygame.draw.rect(screen, dark_red, pygame.Rect(30, 30, 60, 60))
 revbox = pygame.draw.rect(screen, dark_red, pygame.Rect(x-120,y-120, 60,30))
 revboxVal = False
@@ -42,6 +36,11 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
     screen.fill(fuchsia)  # Transparent background
-    #rev = movement.update(rev)
-    pygame.draw.rect(screen, dark_red, logo)
+    win32gui.SetWindowPos(hwnd,win32con.HWND_TOPMOST,0,0,0,0,0x0001)
+    #for x in pointarr:
+    #    pygame.draw.rect(screen,dark_red,pygame.Rect(x,(20,20)))
+    revbox = movement.RevUpdate(revbox,revboxVal)
+    rev,goal = movement.update(rev,goal,revbox)
+    pygame.draw.rect(screen, dark_red, rev)
+    pygame.draw.rect(screen, dark_red,revbox)
     pygame.display.update()
