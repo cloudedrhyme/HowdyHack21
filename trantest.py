810e72e1a136e2005c933e0486ff7e4b3eaa106e
cloudedrhyme <74265937+cloudedrhyme@users.noreply.github.com>
2021-09-12 10:22:19 -0500

    bed and laying images

--- trantest.py
+++ trantest.py
@@ -36,11 +36,6 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
     screen.fill(fuchsia)  # Transparent background
-    win32gui.SetWindowPos(hwnd,win32con.HWND_TOPMOST,0,0,0,0,0x0001)
-    #for x in pointarr:
-    #    pygame.draw.rect(screen,dark_red,pygame.Rect(x,(20,20)))
-    revbox = movement.RevUpdate(revbox,revboxVal)
-    rev,goal = movement.update(rev,goal,revbox)
+    #rev = movement.update(rev)
     pygame.draw.rect(screen, dark_red, rev)
-    pygame.draw.rect(screen, dark_red,revbox)
     pygame.display.update()
