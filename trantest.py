84a70937e5618dbbcdc26dae962b2cb78f09a7a5
Clewn <47167053+Clewn@users.noreply.github.com>
2021-09-11 22:25:17 -0500

    Adding Movement file and fullscreen function
    title

--- trantest.py
+++ trantest.py
@@ -2,9 +2,12 @@ import pygame
 import win32api
 import win32con
 import win32gui
+import movement
 
 pygame.init()
-screen = pygame.display.set_mode((800, 600), pygame.NOFRAME)
+flags = pygame.NOFRAME
+screen = pygame.display.set_mode((0,0), flags)
+x,y = pygame.display.get_window_size()
 done = False
 fuchsia = (255, 0, 128)  # Transparency color
 dark_red = (139, 0, 0)
@@ -15,6 +18,8 @@ win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE,
                        win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)
 # Set window transparency color
 win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*fuchsia), 0, win32con.LWA_COLORKEY)
+
+movement.test(x,y)
 
 while not done:
     for event in pygame.event.get():
