cbcd26a30fc9ce3c6e566f1d49365099adb0fe78
cloudedrhyme <74265937+cloudedrhyme@users.noreply.github.com>
2021-09-12 11:37:18 -0500

    Walking animation complete

--- trantest.py
+++ trantest.py
@@ -6,9 +6,12 @@ import movement
 import random
 import player
 from pygame import mixer  # Load the popular external library
+
 # mixer.init()
 # mixer.music.load('C:/Users/Clown/Downloads/Rick_Roll.mp3')
 # mixer.music.play()
+
+
 pygame.init()
 flags = pygame.NOFRAME
 screen = pygame.display.set_mode((0,0), flags)
@@ -18,13 +21,15 @@ goal = list(random.choice(pointarr))
 done = False
 fuchsia = (255, 0, 128)  # Transparency color
 dark_red = (139, 0, 0)
+
 # Create layered window
 hwnd = pygame.display.get_wm_info()["window"]
 win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE,
                        win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)
 # Set window transparency color
 win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*fuchsia), 0, win32con.LWA_COLORKEY)
-rev = pygame.draw.rect(screen, dark_red, pygame.Rect(30, 30, 60, 60))
+
+rev = player.Player(pygame.draw.rect(screen, dark_red, pygame.Rect(30, 30, 60, 60)))
 revbox = pygame.draw.rect(screen, dark_red, pygame.Rect(x-120,y-120, 60,30))
 
 all_sprites = pygame.sprite.Group()
@@ -43,6 +48,10 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
     screen.fill(fuchsia)  # Transparent background
-    #rev = movement.update(rev)
-    pygame.draw.rect(screen, dark_red, rev)
+    win32gui.SetWindowPos(hwnd,win32con.HWND_TOPMOST,0,0,0,0,0x0001)
+    revbox = movement.RevUpdate(revbox,revboxVal)
+    rev,goal,revMovement = movement.update(rev,goal,revbox,revMovement)
+    all_sprites.draw(screen)
+    pygame.draw.rect(screen, dark_red,revbox)
+    rev.update()
     pygame.display.update()
